5c22717afe1083327b52b2bacfaf598b09184586
Garfunkle <T.blin@outlook.fr>
2022-05-09 18:54:13 +0200

    add asset map

---
+++ Tiled/accessoire_castel.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="accessoire_castel" tilewidth="16" tileheight="16" tilecount="660" columns="30">
+ <image source="../../../tileset_castel.png" trans="000000" width="480" height="360"/>
+</tileset>
---
+++ Tiled/walls_castel.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="walls_castel" tilewidth="16" tileheight="16" tilecount="1088" columns="34">
+ <image source="../../../tileset_castel-2.png" trans="000000" width="544" height="512"/>
+</tileset>
